817f9850cffa19ec45773c8d70d50df441aaca54
tomoze <42153608+tomoze@users.noreply.github.com>
2020-03-04 23:26:37 +0200

    Update#1
    Added transition between splash screen and menu.

---
+++ Assets/Scripts/SplashToMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashToMenu : MonoBehaviour
+{
+
+    public GameObject Logo;
+    public GameObject Text;
+
+    void Start()
+    {
+        StartCoroutine(RunSplash());
+    }
+
+    IEnumerator RunSplash()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Logo.SetActive(true);
+        Text.SetActive(true);
+        yield return new WaitForSeconds(3.75f);
+        SceneManager.LoadScene(1);
+    }
+}
