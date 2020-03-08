13db2413af358a0a5bb3e4fc6b2bde74259b7fce
tomoze <42153608+tomoze@users.noreply.github.com>
2020-03-09 01:58:41 +0200

    Update3

--- Assets/Scripts/SplashToMenu.cs
+++ Assets/Scripts/SplashToMenu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashToMenu : MonoBehaviour
 {
 
     public GameObject Logo;
     public GameObject Text;
+    public GameObject BG;
 
     void Start()
     {
@@ -16,10 +18,17 @@ public class SplashToMenu : MonoBehaviour
 
     IEnumerator RunSplash()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.75f);
+        //FadeIn();
         Logo.SetActive(true);
         Text.SetActive(true);
-        yield return new WaitForSeconds(3.75f);
+        BG.SetActive(true);
+        yield return new WaitForSeconds(2.25f);
         SceneManager.LoadScene(1);
     }
+
+    //void FadeIn()
+    //{
+    //    BG.CrossFadeAlpha(1.0f, 1.5f, false);
+    //}
 }
