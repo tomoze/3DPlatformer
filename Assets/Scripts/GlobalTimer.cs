5bda6b99abce7eba63ce4f6c4e1fff8eb3d330f7
adogur <61784297+adogur@users.noreply.github.com>
2020-04-05 19:36:09 +0300

    pridetas timeris

---
+++ Assets/Scripts/GlobalTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalTimer : MonoBehaviour
+{
+    public GameObject timeDisplay;
+    public bool eina = false;
+    public int sekundes = 100;
+
+
+    void Update()
+    {
+
+        if (eina == false)
+        {
+            StartCoroutine(atimtiSekunde());
+        }
+
+    }
+
+    IEnumerator atimtiSekunde()
+    {
+        eina = true;
+        sekundes -= 1;
+        timeDisplay.GetComponent<Text>().text = "" + sekundes;
+        yield return new WaitForSeconds(1);
+        eina = false;
+    }
+}
